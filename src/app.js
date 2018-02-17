d9d2f7da6ff797cee61e9009415df01bdb2b4401
Deguang <better_li@live.com>
2018-02-17 15:56:07 +0800

    init package.json

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const appComponent = require('app');
+
+
+
+
+new app = Vue({
+
+})
---
+++ src/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="my-app">
+    {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+      msg: 'ssr demo'
+  }
+}
+</script>
+
