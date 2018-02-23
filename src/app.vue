5cc0a2eb662e50042e9ab07dd77d9a86a316cb24
Deguang <better_li@live.com>
2018-02-23 10:07:21 +0800

    init vue components

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="my-app">
-    {{msg}}
+    <h3>{{ title }}</h3>
+    <diy-cmp :name="name"></diy-cmp>
   </div>
 </template>
 <script>
+import DiyCmp from './DiyCmp'
+
 export default {
+  components: {
+    DiyCmp
+  },
   data() {
-      msg: 'ssr demo'
+    return {
+      title: 'A demo of Vue server-side-render.',
+      name: 'Deguang'
+    }
+  },
+  created() {
+    console.log("app component created.");
   }
 }
 </script>
+<style scoped>
+  h3 {
+    color: #45b6a4;
+  }
+</style>
+
 
